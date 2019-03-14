9fe16e89b3c253c6b3270c06612c6466bde396ad
João Victor Fernandes <lfvictorf@gmail.com>
2019-03-14 11:15:49 -0300

    Added project roadmap

---
+++ project-roadmap.c
@@ -0,0 +1,15 @@
+/* Code rodemap
+1- Implement a simple tcp client
+2- Implement a simple tcp server
+3- Establish the connection between computers in the same network
+4- Establish the connection between computers in different networks
+*/
+
+/* Key points:
+1- Measure elapsed time between requests and create a log for them.
+*/
+
+/* Main questions:
+1- Can there be collisions between requests?
+2- How to deal with concurrency?
+*/
